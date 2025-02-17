b6cdb377f4999145d8f3bec7d37dbcceca7bb5c0
Egor Panichkin <gogaspb98@gmail.com>
2025-02-17 18:15:36 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// var a
+// console.log(a)
+// var b = Number(prompt("Введите 2 число"))
+// console.log(b)
+
+// var c = a + b
+// console.log(c)
+
+var bool = false
+console.log(typeof bool)
+
+var a = -12
+var b = 12
+
+console.log(NaN <= b)
